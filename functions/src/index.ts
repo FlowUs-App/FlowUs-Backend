0de8e391414e94885ea2f57b69c11f8a44824617
lucas-goldner <lucas.goldner@googlemail.com>
2022-05-14 23:49:01 +0200

    refactor: Move function into own file

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,11 +1,3 @@
-import * as functions from "firebase-functions";
+import helloWorldHandler from "./helloworld";
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-
-exports.helloWorld = functions.region("europe-west1").https.onCall(() => {
-  functions.logger.info("Hello logs!", { structuredData: true });
-
-  return { text: "Hello from Firebase!" };
-});
+exports.helloWorld = helloWorldHandler;
