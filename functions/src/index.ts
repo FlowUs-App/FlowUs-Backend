2a44585d6987f26ed2c88b54e01c213f68d4cc53
lucas-goldner <lucas.goldner@googlemail.com>
2022-05-14 21:39:25 +0200

    feat: Change region from us to europe

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -4,7 +4,7 @@ import * as functions from "firebase-functions";
 // // https://firebase.google.com/docs/functions/typescript
 //
 
-exports.helloWorld = functions.https.onCall(() => {
+exports.helloWorld = functions.region("europe-west1").https.onCall(() => {
   functions.logger.info("Hello logs!", { structuredData: true });
 
   return { text: "Hello from Firebase!" };
